8a9858beafb3a7cd53a226973ad87439e9bfc5a7
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2024-04-13 22:20:17 +0530

    windcontacts added

---
+++ app/chats/ChatContacts.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import users from '../data/users'
+
+export default function chatContacts() {
+  return (
+    <div className="flex flex-col w-2/6">
+        {users.map((e, i) => (
+          <div className=" p-2 hover:bg-slate-600 border-2 border-slate-900 flex gap-4 items-center">
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS chat bubble component" src={e.imgURL} />
+              </div>
+            </div>
+            <div>{e.name}</div>
+          </div>
+        ))}
+      </div>
+  )
+}
