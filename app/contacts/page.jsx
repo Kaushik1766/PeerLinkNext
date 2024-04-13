3113e518842424e91821e141288136d687ebc8a2
yashunh <priyanshuntak@gmail.com>
2024-04-13 19:16:45 +0530

    hgjghj

---
+++ app/contacts/page.jsx
@@ -0,0 +1,14 @@
+export default function HomeLeft() {
+    return (
+        <div className=" flex justify-start border-1 rounded-xl border-slate-600">
+            <div className="avatar">
+                <div className="w-12 rounded-full">
+                    <img className="w-12 rounded-full" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+            </div>
+            <div className="pt-2 pl-2 text-lg font-medium">
+                Ayushi
+            </div>
+        </div>
+    )
+}
