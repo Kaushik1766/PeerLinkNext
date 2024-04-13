2d52d2964a8ef77bb93e90643a4098ed70583ab5
Kaushik Saha <116095138+Kaushik1766@users.noreply.github.com>
2024-04-13 23:37:47 +0530

    data update

--- app/data/users.js
+++ app/data/users.js
@@ -2,75 +2,101 @@ const { deflate } = require("zlib");
 
 const users = [
   {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
+    name: 'user1',
+    chats: [
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+    ]
   },
   {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
+    name: 'user2',
+    chats: [
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+    ]
   },
   {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
+    name: 'user2',
+    chats: [
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        sent: 'asdfas'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+      {
+        received: 'afsdfasdf'
+      },
+    ]
   },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "aurat",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "ladki",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "ladki",
-  },
-  {
-    imgURL:
-      "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg",
-    name: "ladki",
-  },
-];
+]
 
 export default users;
