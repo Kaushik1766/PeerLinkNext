d68e409d206c247c891da46d51273dd54005998b
Prateek195jain <118386573+Prateek195jain@users.noreply.github.com>
2024-04-15 00:59:40 +0530

    fixed adf

--- app/login/Signup.jsx
+++ app/login/Signup.jsx
@@ -1,77 +1,102 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { setCookie } from 'cookies-next'
-import { useDispatch } from 'react-redux'
+import React, { useState } from "react";
+import axios from "axios";
+import { setCookie } from "cookies-next";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "@/redux/features/user";
 
 function Signup() {
-    const [otp, setOtp] = useState(0)
-    const [preuid, setUid] =useState('')
-    const dispatch = useDispatch()
+  const [otp, setOtp] = useState(0);
+  const [preuid, setUid] = useState("");
+  const dispatch = useDispatch();
 
-    async function submitForm(formdata) {
-        if(otp==0){
-            let uid = formdata.get('uid')
-            let password = formdata.get('password')
-            setUid(uid)
-            axios.post('http://localhost:4000/register', {
-                uid: uid,
-                password: password
-            }).then((res) => {
-                console.log(res)
-                if (res.data.msg == "user registered") {
-                    setOtp(otp+1)
-                }
-                else {
-                    window.alert(res.data.msg)
-                }
-            })
-        }
-        else if(otp==1){
-            let otp = formdata.get('otp')
-            let uid = preuid
-            console.log(uid)
-            axios.post('http://localhost:4000/otp', {
-                uid: uid,
-                otp: otp
-            }).then((res) => {
-                console.log(res)
-                if (res.data.msg == "email verified") {
-                    setCookie('uid',uid,{  maxAge: 60 * 60 * 24 })
-                    dispatch(setCurrentUser(uid))
-                    window.location.href = "http://localhost:3000/chats"
-                }
-                else {
-                    window.alert(res.data.msg)
-                }
-            })
-        }
+  async function submitForm(formdata) {
+    if (otp == 0) {
+      let uid = formdata.get("uid");
+      let password = formdata.get("password");
+      setUid(uid);
+      axios
+        .post("http://localhost:4000/register", {
+          uid: uid,
+          password: password,
+        })
+        .then((res) => {
+          console.log(res);
+          if (res.data.msg == "user registered") {
+            setOtp(otp + 1);
+          } else {
+            window.alert(res.data.msg);
+          }
+        });
+    } else if (otp == 1) {
+      let otp = formdata.get("otp");
+      let uid = preuid;
+      console.log(uid);
+      axios
+        .post("http://localhost:4000/otp", {
+          uid: uid,
+          otp: otp,
+        })
+        .then((res) => {
+          console.log(res);
+          if (res.data.msg == "email verified") {
+            setCookie("uid", uid, { maxAge: 60 * 60 * 24 });
+            dispatch(setCurrentUser(uid));
+            window.location.href = "http://localhost:3000/chats";
+          } else {
+            window.alert(res.data.msg);
+          }
+        });
     }
+  }
 
-    return (
-        <form action={submitForm} className='flex flex-col mt-5 p-10 gap-8'>
-            {otp == 0 && (
-                <>
-                    <div className='flex flex-col gap-2'>
-                        <label htmlFor="UID" className=''>Enter your UID or Username</label>
-                        <input type="text" name='uid' placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                    </div>
-                    <div className='flex flex-col gap-2'>
-                        <label htmlFor="UID" className=''>Enter Password</label>
-                        <input type="text" name='password' placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                    </div>
-                </>
-            )}
-            {otp == 1 && (
-                <>
-                    <div className='flex flex-col gap-2'>
-                        <label htmlFor="OTP" className=''>Enter OTP</label>
-                        <input type="text" name='otp' placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                    </div>
-                </>
-            )}
-            <button type='submit' className='btn btn-primary w-1/2'>Submit</button>
-        </form>
-    )
+  return (
+    <form action={submitForm} className="flex flex-col mt-5 p-10 gap-8">
+      {otp == 0 && (
+        <>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="UID" className="">
+              Enter your UID or Username
+            </label>
+            <input
+              type="text"
+              name="uid"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="UID" className="">
+              Enter Password
+            </label>
+            <input
+              type="text"
+              name="password"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+        </>
+      )}
+      {otp == 1 && (
+        <>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="OTP" className="">
+              Enter OTP
+            </label>
+            <input
+              type="text"
+              name="otp"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+        </>
+      )}
+      <button type="submit" className="btn btn-primary w-1/2">
+        Submit
+      </button>
+    </form>
+  );
 }
 
-export default Signup
+export default Signup;
